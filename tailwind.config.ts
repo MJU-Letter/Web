5f27ec37371f80693b3ff73694c47b4b8df547e9
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-01-30 18:09:50 +0900

    chore:#1 디자인 시스템

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,13 @@
 import type { Config } from "tailwindcss";
 
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) } as any;
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) } as any;
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) } as any;
+const px0_1000 = { ...Array.from(Array(1001)).map((_, i) => `${i}px`) } as any;
+const rem0_5 = {
+  ...Array.from(Array(60)).map((_, i) => `${i / 10}rem`),
+} as any;
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +16,41 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      borderWidth: px0_10,
+      fontSize: px0_200,
+      lineHeight: px0_100,
+      width: px0_1000,
+      height: px0_1000,
+      maxWidth: px0_1000,
+      maxHeight: px0_1000,
+      minWidth: px0_1000,
+      minHeight: px0_1000,
+      spacing: px0_200,
+      borderRadius: px0_100,
+      colors: {
+        "main-background": "var(--main-background)",
+        "main-point": "var(--main-point)",
+        "main-white": "var(--main-white)",
+        "point-red": "var(--point-red)",
+        "point-green": "var(--point-green)",
+        "point-yellow": "var(--point-yellow)",
+        "point-blue": "var(--point-blue)",
+        "gray-dark-text-1": "var(--gray-dark-text-1)",
+        "gray-medium-dark": "var(--gray-medium-dark)",
+        "gray-medium-text-2": "var(--gray-medium-text-2)",
+        "gray-light-medium": "var(--gray-light-medium)",
+        "gray-light": "var(--gray-light)",
+        "basic-red": "var(--basic-red)",
+        "basic-orange": "var(--basic-orange)",
+        "basic-green": "var(--basic-green)",
+        "basic-blue": "var(--basic-blue)",
+      },
+
+      screens: {
+        tablet: "329px",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss")],
 };
 export default config;
