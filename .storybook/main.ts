10a6bb5a89fc7a97493a371ea7974ad38435b2b4
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-01-30 18:11:12 +0900

    chore:#1 storybook 설정

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  // public 폴더에서 정적 파일 불러오기
+  staticDirs: ["../public"],
+};
+export default config;
