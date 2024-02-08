335228a40df59e2063f53a7029108c307bfe27d7
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-02-09 00:16:31 +0900

    feat:#3 /mypage, /notice

---
+++ src/app/notice/page.tsx
@@ -0,0 +1,12 @@
+import Notice from "@/components/templates/notice/Notice";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Notice />
+    </div>
+  );
+};
+
+export default page;
