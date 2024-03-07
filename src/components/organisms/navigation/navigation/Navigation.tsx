0fa64a9ad3a6ff152668e49d572087ea071a12b4
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-03-07 23:44:19 +0900

    chore:#9 chore

--- src/components/organisms/navigation/navigation/Navigation.tsx
+++ src/components/organisms/navigation/navigation/Navigation.tsx
@@ -4,17 +4,15 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import logo from "@public/icons/logo.png";
 import menu from "@public/icons/menu.png";
-
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import NavigationList from "../navigationList/NavigationList";
 
 import ModalPortal from "@/utils/ModalPortal";
-import useDeviceType from "@/hooks/useDeviceType";
 
 const Navigation = () => {
   const [isOpened, setIsOpened] = useState(false);
-  const deviceType = useDeviceType();
+
   const handleIsOpened = () => {
     setIsOpened(!isOpened);
   };
@@ -52,27 +50,6 @@ const Navigation = () => {
           </div>
         </div>
       </div>
-      {/* {isOpened && (
-        <div className="relative">
-          <NavigationList isOpened={isOpened} setIsOpened={setIsOpened} />
-        </div>
-      )}
-      <div className="fixed z-header  flex items-center w-360 justify-between h-56 py-12 px-16 bg-main-background box-border">
-        <Link href={"/"}>
-          <Image className="w-67 h-32" src={logo} alt="logo" />
-        </Link>
-        <div className="flex">
-          <Link href={"/notice"}>
-            <NoticeIcon isOn={false} />
-          </Link>
-          <Image
-            className="ml-12 w-24 h-24 cursor-pointer"
-            onClick={handleIsOpened}
-            src={menu}
-            alt="menu"
-          />
-        </div>
-      </div> */}
     </>
   );
 };
