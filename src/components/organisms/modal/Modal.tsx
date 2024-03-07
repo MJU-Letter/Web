6acb48c71e0cace05370c6bf1a4b17632184b060
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-03-07 23:43:49 +0900

    chore: #9 chore

--- src/components/organisms/modal/Modal.tsx
+++ src/components/organisms/modal/Modal.tsx
@@ -1,13 +1,13 @@
 import ModalView from "../../molecules/modal/ModalView";
 import { modalProps } from "@/types/Modal";
 import React from "react";
-import DefaultModal from "../../molecules/modal/ModalContent";
 
 const Modal = ({ info, yesBtn, noBtn }: modalProps) => {
   return (
-    <ModalView>
-      <DefaultModal info={info} yesBtn={yesBtn} noBtn={noBtn} />
-    </ModalView>
+    <div>d</div>
+    // <ModalView>
+    //   <DefaultModal info={info} yesBtn={yesBtn} noBtn={noBtn} />
+    // </ModalView>
   );
 };
 
