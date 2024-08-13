c849f1199d687fa9eb45ef89cb24815ef862f910
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-08-13 10:27:44 +0900

    chore: chore

--- src/app/setting/profile/page.tsx
+++ src/app/setting/profile/page.tsx
@@ -1,18 +1,18 @@
-"use client";
-import ProfileSetting from "@/components/organisms/profileSetting/ProfileSetting";
-import React from "react";
-import testProfile '../../../'
+'use client';
+import ProfileSetting from '@/components/organisms/profileSetting/ProfileSetting';
+import React from 'react';
+import testProfile from '../../../testProfile.png';
 
 const page = () => {
   const test = () => {
-    console.log("hi");
+    console.log('hi');
   };
   return (
     <div>
       <ProfileSetting
-        name={"이보현"}
-        major={"응용소프트웨어전공"}
-        studentId={"19"}
+        name={'이보현'}
+        major={'응용소프트웨어전공'}
+        studentId={'19'}
         profileImg={testProfile}
         setInstagramId={test}
       />
