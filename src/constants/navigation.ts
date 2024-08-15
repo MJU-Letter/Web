c865fe8109e5351df429435f21f50b260436cedb
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-08-15 14:44:33 +0900

    feat: nagivation constants 생성

---
+++ src/constants/navigation.ts
@@ -0,0 +1,27 @@
+export const NAVIGATION = {
+  login: {
+    title: 'login',
+    path: '/',
+  },
+  social: {
+    title: 'social',
+    main_path: '/mju-letter/main',
+    detail_path: '/mju-letter/detail',
+  },
+  auth: {
+    title: 'auth',
+    path: '/auth',
+  },
+  mypage: {
+    title: 'my page',
+    path: '/mju-letter/mypage',
+  },
+  notice: {
+    title: 'notice',
+    path: '/mju-letter/notice',
+  },
+  setting: {
+    title: 'setting',
+    path: '/mju-letter/setting',
+  },
+};
