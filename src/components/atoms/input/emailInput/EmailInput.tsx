94360d7e9b38ccd74793458158efffc6fb7103dd
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-08-16 23:57:27 +0900

    feat: input component 생성

--- src/components/atoms/input/emailInput/EmailInput.tsx
+++ src/components/atoms/input/emailInput/EmailInput.tsx
@@ -1,16 +1,21 @@
+'use client';
 import React from 'react';
 
 type Props = {
   label: string;
   placeholder: string;
   isDisabled: boolean;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const EmailInput = ({ label, placeholder, isDisabled }: Props) => {
+const EmailInput = ({ label, placeholder, isDisabled, value, onChange }: Props) => {
   return (
     <div className={`flex items-center whitespace-nowrap ${isDisabled ? 'opacity-50 pointer-events-none' : ''}`}>
       <h1 className={`w-232 medium-bold text-main-point mr-25`}>{label}</h1>
       <input
+        value={value}
+        onChange={onChange}
         className='appearance-none focus:outline-none focus:ring-0 border-b-1 border-b-main-point placeholder:text-gray-medium-text-2 medium-regular pb-8 px-4 pt-4'
         placeholder={placeholder}
         disabled={isDisabled}
--- src/components/atoms/letter/Letter.stories.tsx
+++ src/components/atoms/letter/Letter.stories.tsx
@@ -1,12 +1,12 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import test from "../../../../public/icons/testProfile.png";
+import type { Meta, StoryObj } from '@storybook/react';
+import test from '../../../../public/icons/testProfile.png';
 
-import Letter from "./Letter";
+import Letter from './Letter';
 
 const meta: Meta<typeof Letter> = {
-  title: "components/atoms/letter/Letter",
+  title: 'components/atoms/letter/Letter',
   component: Letter,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   argTypes: {},
 };
 
@@ -16,12 +16,12 @@ type Story = StoryObj<typeof Letter>;
 
 export const DefaultMinLetter: Story = {
   args: {
-    letterText: "우리 싸우지 말고 사이 좋게 지내자~~~",
+    letterText: '우리 싸우지 말고 사이 좋게 지내자~~~',
     user: {
-      name: "이보현",
+      name: '이보현',
       profileImg: test,
     },
-    backgroundColor: "#8ED5BE",
+    backgroundColor: 'green',
     isMine: false,
   },
 };
@@ -29,16 +29,16 @@ export const DefaultMinLetter: Story = {
 export const DefaultMaxLetter: Story = {
   args: {
     letterText:
-      "그 많은 마이크를 저희가 다 사용한게 아그 많은 마이크를 저희가 다 사용한게 아니라서 저희 잘못이 아닐 수도 있그 많은 마이크를 저희가 다 사용한게 아니라서 저희 잘못이 아닐 수도 있희가 다 사용한게 아니라서 저희 잘못이 아닐 수도 있 그 많은 마이크를 저희가 다 사용한게 아니라서 저희 잘못이 아닐 수도 있",
-    backgroundColor: "#F39889",
+      '그 많은 마이크를 저희가 다 사용한게 아그 많은 마이크를 저희가 다 사용한게 아니라서 저희 잘못이 아닐 수도 있그 많은 마이크를 저희가 다 사용한게 아니라서 저희 잘못이 아닐 수도 있희가 다 사용한게 아니라서 저희 잘못이 아닐 수도 있 그 많은 마이크를 저희가 다 사용한게 아니라서 저희 잘못이 아닐 수도 있',
+    backgroundColor: 'red',
     isMine: false,
   },
 };
 
 export const MyLetter: Story = {
   args: {
-    letterText: "우리 싸우지 말고 사이 좋게 지내자~~~ ",
-    backgroundColor: "#8ED5BE",
+    letterText: '우리 싸우지 말고 사이 좋게 지내자~~~ ',
+    backgroundColor: 'green',
     isMine: true,
   },
 };
